43fb5c924bbf76119f55e6596436c01bb712ed9b
AumMule <ommule2005@gmail.com>
2025-07-07 08:45:56 +0530

    made a basic pomodoro app with basic ui and start stop reset functionality

--- script.js
+++ script.js
@@ -1,20 +1,48 @@
-async function fetchData() {
-  try {
-    document.getElementById('quote').textContent = 'Loading...';
-    document.getElementById('author').textContent = '';
-    const response = await fetch('https://dummyjson.com/quotes/random');
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
+let totalSeconds = 1500;
+let timer = document.querySelector(".pomodoro-timer h1");
+let startBtn = document.querySelector(".start-timer");
+let pauseBtn = document.querySelector(".pause-timer");
+let resetBtn = document.querySelector(".reset-timer");
+
+let isOn = false;
+let timerId; // store the interval ID here
+
+function updateTime() {
+    let minutes = Math.floor(totalSeconds / 60);
+    let seconds = totalSeconds % 60;
+    if (seconds < 10) {
+        seconds = `0${seconds}`;
     }
-    const data = await response.json();
-    document.getElementById('quote').textContent = data.quote;
-    document.getElementById('author').textContent = `- ${data.author}`;
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    document.getElementById('quote').textContent = 'Failed to load quote.';
-    document.getElementById('author').textContent = '';
-  }
+    timer.innerHTML = `${minutes}:${seconds}`;
 }
 
+startBtn.addEventListener("click", () => {
+    if (!isOn) {
+        timerId = setInterval(() => {
+            totalSeconds--;
+            updateTime();
 
-qoute = ["hello bro","Nigga you"]
+            if (totalSeconds <= 0) {
+                clearInterval(timerId);
+                isOn = false;
+            }
+        }, 1000);
+        isOn = true;
+    }
+});
+
+pauseBtn.addEventListener("click", () => {
+    if (isOn) {
+        clearInterval(timerId);
+        isOn = false;
+    }
+});
+
+
+resetBtn.addEventListener("click",()=>{
+    if(!isOn){
+        clearInterval(timerId);
+        totalSeconds=1500;
+        updateTime();
+    }
+});
